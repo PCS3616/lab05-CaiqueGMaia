7418e6059a31f71a47e5c0384ed03db8c3958e36
CaiqueGMaia <86488768+CaiqueGMaia@users.noreply.github.com>
2025-02-21 12:43:33 -0300

    Add files via upload

---
+++ submission/fatorial.asm
@@ -0,0 +1,30 @@
+        @   /0000
+        LD  N
+        MM  RES
+        SB  UM
+        MM  I
+        LD  N
+        SC  /012
+        JZ  /010
+        JP  /00A
+        HM  /00F
+        
+        @   /0014
+        LD  RES
+        ML  I
+        MM  RES
+        LD  I
+        SB  UM
+        MM  i
+        RS  /012
+
+        @   /0100
+N       K   /0008
+RES     K   /0000
+ZERO    K   /0000
+UM      K   /0001
+I       K   /0000   
+
+
+
+
